66bf1634d01c091a433fc098ea5c51b05c93c83c
akinpinkman <akinbilgin54@hotmail.com>
2024-07-21 17:32:57 +0300

    Add header component and update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameModule } from './game/game.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GameModule],
+  imports: [RouterOutlet, GameModule, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-exercise';
