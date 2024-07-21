65722f6c7da676353d5bfccc8ebec7a21c2225b3
akinpinkman <akinbilgin54@hotmail.com>
2024-07-21 19:16:51 +0300

    Add header component and sidebar functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { SidebarService } from '../../services/sidebar.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isSidebarOpen$ = this.sidebarService.isSidebarOpen$;
+
+  constructor(private sidebarService: SidebarService) {}
+
+  toggleSidebar() {
+    this.sidebarService.toggleSidebar();
+  }
+}
--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SidebarService {
+  private isSidebarOpen = new BehaviorSubject<boolean>(false);
+  isSidebarOpen$ = this.isSidebarOpen.asObservable();
 
-  constructor() { }
+  toggleSidebar() {
+    this.isSidebarOpen.next(!this.isSidebarOpen.value);
+  }
 }
