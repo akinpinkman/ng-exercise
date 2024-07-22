67f964d51ca2b12c8e58bf549ca7919a017988a4
akinpinkman <akinbilgin54@hotmail.com>
2024-07-22 10:51:31 +0300

    Update app component to include sidebar functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameModule } from './game/game.module';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GameModule, HeaderComponent],
+  imports: [RouterOutlet, GameModule, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
